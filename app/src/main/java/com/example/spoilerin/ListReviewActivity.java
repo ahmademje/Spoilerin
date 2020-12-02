a972d51f27b82a23ab32445b86e80c91e9c0c8a9
ahmad <ahmadjihad.maulana@gmail.com>
2020-12-02 11:21:19 +0700

    ListReview

---
+++ app/src/main/java/com/example/spoilerin/ListReviewActivity.java
@@ -0,0 +1,16 @@
+package com.example.spoilerin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListReviewActivity extends AppCompatActivity {
+
+    String users[], reviews[];
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_review);
+    }
+}
